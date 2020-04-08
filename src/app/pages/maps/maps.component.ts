d981cc73b1595c0fb08f90dc3e6c64cea3675da4
Hugo Henrique <hugohralm@gmail.com>
2020-04-08 02:35:14 -0300

    Adicionando serviço de busca de denuncia, adicionando denuncias ao mapa

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,4 +1,6 @@
+import { Denuncia } from './../../models/denuncia.model';
 import { Component, OnInit } from '@angular/core';
+import { DenunciaAnonimaService } from 'src/app/services/denuncia-anonima.service';
 declare const google: any;
 
 @Component({
@@ -7,50 +9,61 @@ declare const google: any;
   styleUrls: ['./maps.component.scss']
 })
 export class MapsComponent implements OnInit {
+  denuncias: Denuncia[] = [];
 
-  constructor() { }
+  constructor(
+    private denunciaAnonimaService: DenunciaAnonimaService
+  ) { }
 
   ngOnInit() {
-    let map = document.getElementById('map-canvas');
-    let lat = map.getAttribute('data-lat');
-    let lng = map.getAttribute('data-lng');
+    this.carregarDenunciasAnonimas();
+  }
 
-    var myLatlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        zoom: 12,
+  private carregarDenunciasAnonimas(): void {
+    this.denunciaAnonimaService.getAll().subscribe(
+      denuncias => {
+        this.denuncias = denuncias;
+        this.carregarMapa();
+      }
+    );
+  }
+
+  private carregarMapa(): void {
+    let map = document.getElementById('map-canvas');
+    const lat = map.getAttribute('data-lat');
+    const lng = map.getAttribute('data-lng');
+
+    const myLatlng = new google.maps.LatLng(lat, lng);
+    const mapOptions = {
+        zoom: 17,
         scrollwheel: false,
         center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: [
-          {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},
-          {"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},
-          {"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
-          {"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},
-          {"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},
-          {"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-          {"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
-          {"featureType":"water","elementType":"all","stylers":[{"color":'#5e72e4'},{"visibility":"on"}]}]
-    }
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
     map = new google.maps.Map(map, mapOptions);
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'Hello World!'
-    });
-
-    var contentString = '<div class="info-window-content"><h2>Argon Dashboard</h2>' +
-        '<p>A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</p></div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-    });
+    this.adicionarMarkers(map);
   }
 
+  private adicionarMarkers(map: HTMLElement): void {
+    let marker;
+    const infowindow = new google.maps.InfoWindow;
+
+    this.denuncias.forEach(
+      denuncia => {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(denuncia.latitude, denuncia.longitude),
+          map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(mark) {
+          return function() {
+              infowindow.setContent(denuncia.titulo);
+              infowindow.open(map, mark);
+          };
+        })(marker));
+      }
+    );
+  }
 }
